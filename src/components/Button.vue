0cbd850c8dadc07640b22abcc6384ca353f65db7
Michał Sitkowski <emsitkowski@gmail.com>
2024-05-04 15:37:58 +0200

    feat: create button component

---
+++ src/components/Button.vue
@@ -0,0 +1,24 @@
+<template>
+  <button class="px-6 py-3 rounded-full" :class="buttonVariants[variant as ButtonVariant]">{{ label }}</button>
+</template>
+
+<script setup lang="ts">
+type ButtonVariant = "primary" | "secondary";
+
+const props = defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+  variant: {
+    type: String,
+    required: true,
+    default: "primary",
+  },
+});
+
+const buttonVariants = {
+  primary: "bg-almost-white",
+  secondary: "bg-secondary-plump",
+};
+</script>
