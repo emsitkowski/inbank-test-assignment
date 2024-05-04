edae265e46233dc585b0b666ea09f61422ac5d29
Michał Sitkowski <emsitkowski@gmail.com>
2024-05-04 15:34:50 +0200

    feat: add theme variables to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      "primary-night-violet": "#21093A",
+      "secondary-khaki": "#D9D4A7",
+      "secondary-plump": "#AA93FF",
+      "almost-white": "#fbfbf9",
+    },
     fontFamily: {
       primary: ["Bitter", "sans-serif"],
       secondary: ["Inter", "sans-serif"],
