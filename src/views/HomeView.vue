853f5d29df23c4a2257ce73c7734195dc8a19b8c
Michał Sitkowski <emsitkowski@gmail.com>
2024-05-05 13:36:17 +0200

    feat: add smooth scroll to calculator

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 import Hero from "../components/Hero.vue";
 import Calculator from "../components/Calculator.vue";
+import { RouterLink } from "vue-router";
 </script>
 
 <template>
   <main>
-    <Hero class="mb-10 lg:mb-44" />
-    <Calculator />
+    <a href="#calculate">
+      <Hero class="mb-10 lg:mb-44" />
+    </a>
+    <Calculator id="calculate" />
   </main>
 </template>
