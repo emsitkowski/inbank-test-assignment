f34a70b46356f41eaf65519a018b8f140f219887
Michał Sitkowski <emsitkowski@gmail.com>
2024-05-05 22:45:13 +0200

    feat: add calculator store logic

--- src/store/calculator.ts
+++ src/store/calculator.ts
@@ -1,25 +1,39 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
 
+type ClientDetails = {
+  name: String;
+  surname: String;
+  number: String;
+  email: String;
+  income: String;
+};
+
 export const useCalculatorStore = defineStore("calculator", () => {
   const period = ref<number>(0);
   const amount = ref<number>(0);
-  const clientName = ref<string>("");
+  const clientDetails = ref<ClientDetails>({
+    name: "",
+    surname: "",
+    number: "",
+    email: "",
+    income: "",
+  });
 
   // Save loan period
-  function savePeriod(value: number) {
-    period.value = value;
+  function savePeriod(newValue: number) {
+    period.value = newValue;
   }
 
   // Save loan amount
-  function saveAmount(value: number) {
-    amount.value = value;
+  function saveAmount(newValue: number) {
+    amount.value = newValue;
   }
 
   // Save client name
-  function saveClientName(value: string) {
-    clientName.value = value;
+  function saveClientDetails(details: ClientDetails) {
+    Object.assign(clientDetails.value, details);
   }
 
-  return { period, amount, clientName, savePeriod, saveAmount, saveClientName };
+  return { period, amount, clientDetails, savePeriod, saveAmount, saveClientDetails };
 });
