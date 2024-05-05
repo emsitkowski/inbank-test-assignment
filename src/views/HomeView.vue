e6af077ceaf48db9ce73c8eea68c1ef45b988cab
Michał Sitkowski <emsitkowski@gmail.com>
2024-05-05 13:29:52 +0200

    feat: add calculator to home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Hero from "../components/Hero.vue";
+import Calculator from "../components/Calculator.vue";
 </script>
 
 <template>
   <main>
-    <Hero />
+    <Hero class="mb-10 lg:mb-44" />
+    <Calculator />
   </main>
 </template>
