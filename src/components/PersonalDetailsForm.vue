79b23756fecd8174ae1529a6eeca123fbc54382f
Michał Sitkowski <emsitkowski@gmail.com>
2024-05-05 21:14:44 +0200

    feat: create personal details form component

---
+++ src/components/PersonalDetailsForm.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <Form class="flex flex-col gap-4" :validation-schema="schema" @submit="handleSubmit!">
+      <div class="flex flex-col gap-2" v-for="field in formFields">
+        <Field :type="field.type" :name="field.name" :placeholder="field.placeholder" />
+        <ErrorMessage class="text-error" :name="field.name" />
+      </div>
+
+      <Button class="mt-6" label="Submit" variant="secondary" />
+    </Form>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Button from "../components/Button.vue";
+import { Field, Form, ErrorMessage } from "vee-validate";
+import * as yup from "yup";
+
+type UserDetails = {
+  name: String;
+  surname: String;
+  number: String;
+  email: String;
+  income: String;
+};
+
+// Declare form fields
+const formFields = [
+  {
+    type: "text",
+    name: "name",
+    placeholder: "First name",
+  },
+  {
+    type: "text",
+    name: "surname",
+    placeholder: "Last name",
+  },
+  {
+    type: "text",
+    name: "number",
+    placeholder: "Mobile number",
+  },
+  {
+    type: "text",
+    name: "email",
+    placeholder: "E-mail",
+  },
+  {
+    type: "text",
+    name: "income",
+    placeholder: "Monthly income",
+  },
+];
+
+// User details form validation schema
+const schema = yup.object({
+  name: yup.string().required(),
+  surname: yup.string().required(),
+  number: yup
+    .string()
+    .matches(/^55\d{5,6}$/, "Number must start with 55 and contain 7 or 8 digits")
+    .required(),
+  email: yup.string().email().required(),
+  income: yup.number().moreThan(100).required(),
+});
+
+// Handle form data
+async function handleSubmit(values: UserDetails) {}
+</script>
