c1baaccab8dbfafb284473db63869b15aa7248ee
Michał Sitkowski <emsitkowski@gmail.com>
2024-05-06 21:34:16 +0200

    feat: add base theme shadow to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,11 @@ export default {
     colors: {
       "primary-night-violet": "#21093A",
       "secondary-khaki": "#D9D4A7",
+      "secondary-light-khaki": "#F0F0EA",
       "secondary-plump": "#AA93FF",
       "secondary-lavender": "#E3D2FF",
       "almost-white": "#fbfbf9",
+      white: "#ffffff",
       "light-gray": "#DEDEDE",
       "dark-gray": "#606060",
       error: "#e63946",
@@ -16,6 +18,9 @@ export default {
     fontFamily: {
       primary: ["Bitter", "sans-serif"],
       secondary: ["Inter", "sans-serif"],
+    },
+    boxShadow: {
+      base: "0 0 40px rgba(0, 0, 0, 4%)",
     },
   },
   plugins: [],
