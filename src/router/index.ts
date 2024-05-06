ef7792f6e7864fa3f5b652d3f754727f57dab8b2
Michał Sitkowski <emsitkowski@gmail.com>
2024-05-06 21:38:17 +0200

    feat: add route rules for positive and negative decisions

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,8 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { useRouter, createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import PositiveDecisionView from "../views/PositiveDecisionView.vue";
+import NegativeDecisionView from "../views/NegativeDecisionView.vue";
+import { useCalculatorStore } from "@/store/calculator";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,7 +12,25 @@ const router = createRouter({
       name: "home",
       component: HomeView,
     },
+    {
+      path: "/positive-decision",
+      name: "positive",
+      component: PositiveDecisionView,
+    },
+    {
+      path: "/negative-decision",
+      name: "negative",
+      component: NegativeDecisionView,
+    },
   ],
+});
+
+router.beforeEach((to, from) => {
+  if (!useCalculatorStore().isSuccessfullySubmitted && to.name !== "home") {
+    return { name: "home" };
+  } else {
+    return true;
+  }
 });
 
 export default router;
